1f66451238f35e97d286f35df5b9b3ac165e96df
Wildan M Zaki <wildanmzaki7@gmail.com>
2025-05-24 17:27:09 +0700

    fix: adjust fillable property and another required model data

--- app/Models/FeederDosen.php
+++ app/Models/FeederDosen.php
@@ -11,4 +11,35 @@ class FeederDosen extends Model
 
     protected $table = 'feeder_dosen';
     protected $primaryKey = 'dosen_id';
+
+    const CREATED_AT = 'postdated';
+    const UPDATED_AT = 'updated';
+
+    protected $fillable = [
+        'kode_dosen',
+        'status_dosen',
+        'pt_id',
+        'id_perguruan_tinggi',
+        'kode_perguruan_tinggi',
+        'id_agama',
+        'id_dosen',
+        'id_status_aktif',
+        'jenis_kelamin',
+        'nama_agama',
+        'nama_dosen',
+        'nama_status_aktif',
+        'nidn',
+        'nip',
+        'admin_id',
+        'admin_username',
+        'admin_password',
+        'admin_email',
+        'admincat_id',
+        'admincat_nama',
+        'tanggal_lahir',
+        'updated',
+        'postdated',
+        'updated_id',
+        'posted_id',
+    ];
 }
